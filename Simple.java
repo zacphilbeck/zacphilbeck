c8305aff14cdc3bd32a8f9269ad2254a0bdf981e
zacphilbeck <66897972+zacphilbeck@users.noreply.github.com>
2022-01-25 01:35:45 -0500

    Delete Simple.java

--- Simple.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Simple {
-
-    public static void main(String[] args) {
-        String acctName = "zacphilbeck";
-	    System.out.println("Git account name: " + acctName);
-    }
-}
