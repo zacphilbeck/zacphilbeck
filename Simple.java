b388060cc418a7577dd3d003f3444d8561614ea4
zacphilbeck <66897972+zacphilbeck@users.noreply.github.com>
2022-01-25 01:09:28 -0500

    Initial Simple program commit for username output

---
+++ Simple.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Simple {
+
+    public static void main(String[] args) {
+        String acctName = "zacphilbeck";
+	    System.out.println("Git account name: " + acctName);
+    }
+}
